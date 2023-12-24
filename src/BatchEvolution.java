c4e9cfd26ecf56fabc7b4b8ccfa907bcdf2e7328
codingzn <2946229219@qq.com>
2023-12-25 01:33:32 +0800

    add evolution

---
+++ src/BatchEvolution.java
@@ -0,0 +1,24 @@
+package src;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BatchEvolution {
+    public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException{
+        File file = new File("evolution.txt");
+        FileReader fr = new FileReader(file);
+        BufferedReader bReader = new BufferedReader(fr);
+        String line = bReader.readLine();
+        while (line != null){
+            String[] ags = line.split(" ");
+            System.out.printf("processing %s\n", ags[0]);
+            Evolution.main(ags);
+            line = bReader.readLine();
+        }
+        
+    }
+}
